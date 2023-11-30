ff2c21b6ec92058dc5298908157382d7a722575d
Mshall19 <juandapeva282@gmail.com>
2023-11-29 20:52:59 -0500

    se creo un bosquejo del codigo

---
+++ src/controlador/Ciudades.java
@@ -0,0 +1,6 @@
+package controlador;
+
+public enum Ciudades{
+    Cali, Buenaventura, Palmira, Tulua, Jamundi, Cartago, Zarzal, Candelaria, Pradera, Ginebra, Cerrito, Roldanillo, 
+    Launion, Sevilla, Buga, Guacari, Rozo, Caicedonia, Yumbo, Florida, Dagua
+  }
---
+++ src/vista/VistaGeneral.java
@@ -0,0 +1,16 @@
+package vista;
+
+import controlador.Ciudades;
+import controlador.Partido;
+
+public interface VistaGeneral {
+
+    public void Iniciar();
+    public String getNombre();
+    public String getIdentificacion();
+    public Ciudades getCiudadOrigen();
+    public boolean IsDerecha();
+    public Partido getPartido();
+    public String getPropuestas();
+    
+}
