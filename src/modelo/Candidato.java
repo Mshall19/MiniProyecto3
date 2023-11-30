5946e795705cabf3755d4e5f5e5edddfad2adb73
Kevin-Lorza <kevin.lorza@correounivalle.edu.co>
2023-11-30 09:51:42 -0500

    se modificó la clase candidato

--- src/modelo/Candidato.java
+++ src/modelo/Candidato.java
@@ -4,6 +4,12 @@ import controlador.Ciudades;
 import controlador.Partido;
 
 public class Candidato extends Ciudadano{
+    private String nombre;
+    private String cedula;
+    private String ciudad;
+    private String posicion;
+    private String partido;
+    private int votos;
     private boolean derecha;
     private Partido partido_politico;
     private String propuestas;
@@ -43,5 +49,52 @@ public class Candidato extends Ciudadano{
     public void setNumero_votos(int numero_votos) {
         this.numero_votos = numero_votos;
     }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    public String getCiudad() {
+        return ciudad;
+    }
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+    public String getPosicion() {
+        return posicion;
+    }
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+    public String getPartido() {
+        return partido;
+    }
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+    public int getVotos() {
+        return votos;
+    }
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
 
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre +
+                "\nCédula: " + cedula +
+                "\nCiudad: " + ciudad +
+                "\nPosición: " + posicion +
+                "\nPartido: " + partido +
+                "\nVotos: " + votos +
+                "\nPropuestas: " + propuestas;
+    }
+    
 }
